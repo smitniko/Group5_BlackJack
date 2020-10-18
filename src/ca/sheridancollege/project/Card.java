8fa5f8566cc66616004af7f1dd155b174ed43a98
Nikolas Smith <smitniko@sheridancollege.ca>
2020-10-18 19:22:23 -0400

    built Card class based on designed UML

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -15,18 +15,35 @@ package ca.sheridancollege.project;
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
  * @author dancye
+ * @modifier Nikolas Smith
  */
 public abstract class Card {
-    //default modifier for child classes
-
-    /**
-     * Students should implement this method for their specific children classes
-     * 
-     * // Testing 
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    @Override
+    private String rank;
+    private String suit;
+    private int value;
+    
+    public Card(String rank, String suit, int value){
+        this.rank=rank;
+        this.suit=suit;
+        this.value=value;
+    }
+    
+    public void changeAce(){
+    
+    }
+   
+    public String getRank(){
+        return rank;
+    }
+    
+    public String getSuit(){
+        return suit;
+    }
+    
+    public int getValue(){
+        return value;
+    }   
+    
     public abstract String toString();
-
+ 
 }
