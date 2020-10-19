fa8649b37795d14b5e0baa602ecaf230d3f6fe3d
Nikolas Smith <smitniko@sheridancollege.ca>
2020-10-18 22:13:04 -0400

    updated Card info and added better functionality.

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -18,14 +18,17 @@ package ca.sheridancollege.project;
  * @modifier Nikolas Smith
  */
 public abstract class Card {
-    private String rank;
-    private String suit;
-    private int value;
+    public String rank;
+    public char suit;
+    public int value;
     
-    public Card(String rank, String suit, int value){
-        this.rank=rank;
-        this.suit=suit;
-        this.value=value;
+    private Card(){
+        suit = ' ';
+        value = 0;
+    }
+    
+    public Card(char newSuit, int newValue){
+        this.suit = newSuit;
     }
     
     public void changeAce(){
@@ -37,15 +40,72 @@ public abstract class Card {
     }
     
     public String getSuit(){
+        String suit;	
+        if (this.suit == 'H') {
+                suit = "Hearts";
+        } else if (this.suit == 'S') {
+                suit = "Spades";
+        } else if (this.suit == 'C') {
+                suit = "Clubs";
+        } else if (this.suit == 'D') {
+                suit = "Diamonds";
+        } else {
+                suit = "Unknown";
+        }     
         return suit;
-    }
+            
+        }
     
     public int getValue(){
-        return value;
-    }   
+        return this.value;
+    }
+    
+    public String getValueName(){
+        String name = "Unknown";
+        if (this.value == 1) {		
+                name = "Ace";
+        }
+        else if (this.value == 2) {
+                name = "Two";
+        }
+        else if (this.value == 3) {
+                name = "Three";
+        }
+        else if (this.value == 4) {
+                name = "Four";
+        }
+        else if (this.value == 5) {
+                name = "Five";
+        }
+        else if (this.value == 6) {
+                name = "Six";
+        }
+        else if (this.value == 7) {
+                name = "Seven";
+        }
+        else if (this.value == 8) {
+                name = "Eight";
+        }
+        else if (this.value == 9) {
+                name = "Nine";
+        }
+        else if (this.value == 10) {
+                name = "Ten";
+        }
+        else if (this.value == 11) {
+                name = "Jack";
+        }
+        else if (this.value == 12) {
+                name = "Queen";
+        }
+        else if (this.value == 13) {
+                name = "King";
+        } 
+        return name;
+	}
     
     public String toString(){
-        return "[Rank: " + rank + " Suit: " + suit + " Value " + value +  "]";
+        return getSuit() + " " + this.value;
         
     }
  
