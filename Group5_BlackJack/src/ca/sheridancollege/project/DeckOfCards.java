37579b52328d1b09280aca184519d07fdc361016
Nikolas Smith <smitniko@sheridancollege.ca>
2020-12-07 22:26:18 -0500

    Fixed DeckOfCards class and added functionality.

--- Group5_BlackJack/src/ca/sheridancollege/project/DeckOfCards.java
+++ Group5_BlackJack/src/ca/sheridancollege/project/DeckOfCards.java
@@ -13,9 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 
-public class DeckOfCards {
+public class DeckOfCards{
     private ArrayList<Card> deck;
-    
     
     /**
      * This is a 1 argument constructor that passes in a collection
@@ -69,9 +68,28 @@ public class DeckOfCards {
      */
     public void shuffle(){
         Collections.shuffle(deck);
-        
-        // display
-        System.out.println("Items in deck: " + deck);
+    }
+    
+    // Shows cards left in deck
+    public void display(){
+        for(int i = 0; i < deck.size(); i++)
+        System.out.println("Card #: " + i);
+    }
+    
+    public void cardHit(){
+        boolean hit = true;
+        if(hit == true){
+            //print one card and remove one card from deck
+            System.out.println("You're holding: " + deck.get(0));
+            //show.add(deck.get(0)); // add index to new arrayList
+            //System.out.println("Cards In Hand: " + show);
+            
+            // If there are more than 1 card in hand, show them all (if loop)
+            deck.remove(0); //remove index from deck
+        }else{
+            // display message and print the entire deck
+            System.out.println("You did not Hit. Cards Are: " + deck);
+        }
     }
     
 
